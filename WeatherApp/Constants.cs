09a237505824ba6edfa8f7344f7fc1ef0e8c70fb
xcamif <camifrache4@gmail.com>
2023-01-11 18:51:56 -0300

    Initial commit

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+   public static class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "db058b5a364abbb393dce9d4a62534f9";
+    }
+}
